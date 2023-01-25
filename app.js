d12e98e4c4511b87979462d57fe6ff5e78c4b450
marcelli faria <marcelli.faria2@gmail.com>
2023-01-25 10:48:20 -0300

    voltando com restore para estado anterior

--- app.js
+++ app.js
@@ -1,4 +1,1 @@
-console.log("rodando o sistema de gerenciamento de jogos");
-console.log("aplicação está se conectando ao banco de dados");
-
-console.log("Enviando para o Github")
+console.log("rodando o sistema de gerenciamento de jogos)
