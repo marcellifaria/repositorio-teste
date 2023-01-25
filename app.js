b8ba0fc8560aae1287fa1fc3b2f9c16f776acb24
marcelli faria <marcelli.faria2@gmail.com>
2023-01-25 11:04:06 -0300

    restuarando de novo

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-console.log("rodando o sistema de gerenciamento de jogos)
+console.log("rodando o sistema de gerenciamento de jogos");
+console.log("aplicação está se conectando ao banco de dados");
+
+console.log("Enviando para o Github")
